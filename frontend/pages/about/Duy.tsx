49326eef3e3c3305096b79d86e114a5a80144719
duy nguyen <45688837+duybuin@users.noreply.github.com>
2020-09-08 15:40:13 -0700

    Add files via upload

---
+++ frontend/pages/about/Duy.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Container} from '@components/Layouts';
+import {Text} from  '@component/DataDisplay';
+
+const Duy = () : JSX.Element => {
+    return(
+        <Container align="center">
+        <Text variant="h1">Duy Nguyen</Text>
+        <img src ="/Duy.png" width='375' height = '375' alt = 'Duy' />
+        <Text variant="h4">
+        Undergraduate at San Francisco state university
+        <br />
+        Role: Github master
+        <br />
+        </Text>
+        </Container>
+
+    );
+};
+export default Duy
