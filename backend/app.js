b91739595c7efe191335fb1f8f205341bcdb9016
Nate Munger <mungernate@gmail.com>
2020-10-09 11:56:28 -0700

    added swagger docs, available at /api/docs/

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
+import bodyParser from 'body-parser';
+import swaggerJSdoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -14,6 +19,35 @@ import usersRouter from './routes/users.js';
 import countiesRouter from './routes/counties.js';
 
 var app = express();
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Dream Team Express API",
+      version: "0.1.0",
+      description:
+          "COVID App Swagger Documentation",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Dream Team",
+        url: "https://sfsucsc648.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: process.env.API_URL,
+      },
+    ],
+  },
+  apis: ["./swagger/swagger.yaml"],
+};
+
+const specs = swaggerJSdoc(swaggerOptions);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +62,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users/', usersRouter);
 app.use('/counties', countiesRouter);
+
+app.use(
+    "/docs",
+    swaggerUI.serve,
+    swaggerUI.setup(specs)
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
