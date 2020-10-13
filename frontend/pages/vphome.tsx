d011e2036939257ffe5b4a1ac9e7bc027115a3a0
Nate Munger <mungernate@gmail.com>
2020-10-13 08:32:19 -0700

    added table view

--- frontend/pages/vphome.tsx
+++ frontend/pages/vphome.tsx
@@ -1,18 +1,77 @@
 import React, { useState } from 'react';
+import queryString from 'querystring';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 import { Container } from '@components/Layouts';
-import { Link, Text } from '@components/DataDisplay';
+import { Text } from '@components/DataDisplay';
 import { Button } from '@components/Inputs';
+import County from '../types/County';
 
 const VPHome = (): JSX.Element => {
-  const [infoType, setInfoType] = useState('corona');
+  const [infoType, setInfoType] = useState('all');
   const [county, setCounty] = useState('');
+  const [countyList, setCountyList] = useState<Array<County>>([]);
+
+  async function getCounties() {
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+    const query = {
+      name: county,
+      type: infoType,
+    };
+    const url = `http://${baseURL}/api/counties/?${queryString.stringify(
+      query
+    )}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCountyList(data));
+  }
+
+  type CountyTableProps = {
+    countyArray: Array<County>;
+  };
+
+  const CountyTable = ({ countyArray }: CountyTableProps): JSX.Element => {
+    return (
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>County Name</TableCell>
+              <TableCell>Population</TableCell>
+              <TableCell>Latitude</TableCell>
+              <TableCell>Longitude</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {countyArray.map((countyEntry) => (
+              <TableRow key={countyEntry.id}>
+                <TableCell component="th" scope="row">
+                  {countyEntry.name}
+                </TableCell>
+                <TableCell>{countyEntry.population}</TableCell>
+                <TableCell>{countyEntry.latitude}</TableCell>
+                <TableCell>{countyEntry.longitude}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+
   return (
     <Container align="center">
       <Text variant="h3">Home test for vertical prototype</Text>
       County:
       <input
         id="width"
+        type="text"
         value={county}
         onChange={(event) => setCounty(event.target.value)}
       />
@@ -23,18 +82,15 @@ const VPHome = (): JSX.Element => {
         id="infoType"
         onChange={(event) => setInfoType(event.target.value)}
       >
-        <option value="corona">Corona Virus</option>
-        <option value="fires">Fires</option>
+        <option value="covid">Corona Virus</option>
+        <option value="fire">Fires</option>
+        <option value="all">All</option>
       </select>
       <div>Leave blank for list of all counties.</div>
-      <Link
-        href={{
-          pathname: `/infoPages/${infoType}`,
-          query: { county: `${county}` },
-        }}
-      >
-        <Button variant="secondary">Go</Button>
-      </Link>
+      <Button variant="secondary" onClick={() => getCounties()}>
+        Search
+      </Button>
+      <CountyTable countyArray={countyList} />
     </Container>
   );
 };
