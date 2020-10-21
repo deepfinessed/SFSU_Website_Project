d88db2a64db85bc75cd469e74f197783cf3f1e4b
Nate Munger <mungernate@gmail.com>
2020-10-21 16:23:27 -0700

    Removed redundant middleware

--- backend/app.js
+++ backend/app.js
@@ -68,11 +68,11 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/', indexRouter);
 app.use('/users/', usersRouter);
