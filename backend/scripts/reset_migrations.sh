206d66bc6a7a145a7ce2b761b99d82108695d988
Nate Munger <mungernate@gmail.com>
2020-11-10 11:43:41 -0800

    fixed fatal misnames in fire records

--- backend/scripts/reset_migrations.sh
+++ backend/scripts/reset_migrations.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker-compose exec backend bash -c "rm -rf migrations"
+docker-compose exec backend bash -c "rm -rf prisma/migrations"
 docker-compose exec backend bash -c "node ./utils/truncate_migrations.js"
